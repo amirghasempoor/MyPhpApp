a10358c85f41c2e095da0118a30a5e465bc858a9
amirghasempoor <amirghasempoor79@gmail.com>
2023-04-11 16:16:07 +0430

    create footer and head

--- views/contact.view.php
+++ views/contact.view.php
@@ -1,31 +1,15 @@
-<!DOCTYPE html>
-<html lang="en" class="h-full bg-gray-100">
+<?php require('partials/head.php'); ?>
+<?php require('partials/nav.php'); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
-</head>
-
-<body class="h-full">
-    <div class="min-h-full">
-        
-        <?php require('partials/nav.php'); ?>
-
-        <header class="bg-white shadow">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact Us</h1>
-            </div>
-        </header>
-        <main>
-            <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                Contact Page
-            </div>
-        </main>
+<header class="bg-white shadow">
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
     </div>
+</header>
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        Home Page
+    </div>
+</main>
 
-</body>
-
-</html>
+<?php require('partials/footer.php'); ?>
