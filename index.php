d10813664f88ca7705481c2531ea5175c15b5745
amirghasempoor <amirghasempoor79@gmail.com>
2023-04-12 15:30:57 +0430

    config the connection between db and app

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+require 'Database.php';
+
+$config = require('config.php');
+
+$db = new Database($config);
+
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
