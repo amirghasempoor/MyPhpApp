fc9c82bf9140e9afff85bc816e59a77dd10c200f
amirghasempoor <amirghasempoor79@gmail.com>
2023-04-12 10:53:43 +0430

    create a simple router

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'functions.php';
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+if ($uri === '/') {
+    require 'controllers/index.php';
+}elseif ($uri === '/about') {
+    require 'controllers/about.php';
+}
+elseif ($uri === '/contact') {
+    require 'controllers/contact.php';
+}
