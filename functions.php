6b6ec41afb43ff2e14ceb7f188f3bd99d6e817c0
amirghasempoor <amirghasempoor79@gmail.com>
2023-04-12 13:09:47 +0430

    create a router

--- functions.php
+++ functions.php
@@ -11,3 +11,19 @@ function dd($value)
     echo "</pre>";
     die();
 }
+
+function abort($code = 404){
+    http_response_code($code);
+    require "views/{$code}.php";
+    die();
+}
+
+function routeToController($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)){
+        require $routes[$uri];
+    }
+    else {
+        abort();
+    }
+}
