73379f08267a8ae92d547893bc2d365629ab20f7
amirghasempoor <amirghasempoor79@gmail.com>
2023-04-19 12:18:05 +0430

    create a page for creating notes

--- router.php
+++ router.php
@@ -1,13 +1,7 @@
 <?php
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$routes = require('routes.php');
 
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes.php',
-    '/note' => 'controllers/note.php',
-    '/contact' => 'controllers/contact.php',
-];
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 routeToController($uri, $routes);
