c35002f1733e8e7c8202ae53e708d4580a1959a9
amirghasempoor <amirghasempoor79@gmail.com>
2023-04-19 14:04:55 +0430

    create a form

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -4,7 +4,27 @@
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <h1>create a note</h1>
+        <div>
+            <div class="md:grid md:grid-cols-3 md-gap-6">
+                <div class="mt-5 md:col-span-2 md:mt-0">
+                    <form method="POST">
+                        <div class="space-y-12">
+                            <div class="pb-12">
+                                <div class="col-span-full">
+                                    <label for="about" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
+                                    <div class="mt-2">
+                                        <textarea id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                                    </div>
+                                </div>
+                                <div class="mt-7 text-right">
+                                    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 </main>
 
